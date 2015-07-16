f4501348960958d8d186bfb6f3b839f6ab9e3782
craigkuriger <craig.kuriger@xtra.co.nz>
2015-07-16 23:57:26 +1200

    Website ready for deployment

--- contact.php
+++ contact.php
@@ -1,51 +1,55 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-	$name = trim($_POST["name"]);
-	$email = trim($_POST["email"]);
-	$message = trim($_POST["message"]);
+<?php 
 
-	if ($name == "" OR $email == "" OR $message == "") {
-		echo "You must enter a name, email and message.";
-		exit;
-	}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
+
+    if ($name == "" OR $email == "" OR $message == "") {
+        echo "You must specify a value for name, email address, and message.";
+        exit;
+    }
 
 	// prevent email injection attacks by looking for malicious values
-	foreach($_POST as $ $value) {
-		if (stripos($value, 'Content-Type:') !== FALSE){
-			echo "There was a problem with the infomation you entered.";
-			exit;
-		}
-	}
+    foreach( $_POST as $value ){
+        if( stripos($value,'Content-Type:') !== FALSE ){
+            echo "There was a problem with the information you entered.";    
+            exit;
+        }
+    }
 
-	if ($_POST["address"] != "") {
-		echo "There was a problem with the infomation you entered.";
-	}
+    if ($_POST["address"] != "") {
+        echo "Your form submission has an error.";
+        exit;
+    }
 
-	require_once("include/phpmailer/class.phpmailer.php");
-	$mail = new PHPMailer();
-	if (!$mail->ValidateAddress($email)){
-		echo "You must enter a valid email address";
-		exit;
-	}
+    require_once("include/phpmailer/class.phpmailer.php");
+    $mail = new PHPMailer();
 
-	$email_body = "";
-	$email_body = $email_body . "Name: " . $name . "<br>";
-	$email_body = $email_body . "Email: " . $email . "<br>";
-	$email_body = $email_body . "Message: " . $message;
-	$mail->SetFrom($email, $name);
+    if (!$mail->ValidateAddress($email)){
+        echo "You must specify a valid email address.";
+        exit;
+    }
+
+    $email_body = "";
+    $email_body = $email_body . "Name: " . $name . "<br>";
+    $email_body = $email_body . "Email: " . $email . "<br>";
+    $email_body = $email_body . "Message: " . $message;
+
+    $mail->SetFrom($email, $name);
 	$address = "craig.kuriger@icloud.com";
 	$mail->AddAddress($address, "Leapfrog");
 
 	$mail->Subject    = "Hello From Leapfrog | " . $name;
-	$mail->MsgHTML($email_body);
+ 	$mail->MsgHTML($email_body);
 
-	if(!$mail->Send()) {
-	  echo "Error: Mail not sent" . $mail->ErrorInfo;
-	  exit;
-	}
+    if(!$mail->Send()) {
+      echo "There was a problem sending the email: " . $mail->ErrorInfo;
+      exit;
+    }
 
-	header("Location: contact.php?status=thanks");
-	exit;
+    header("Location: contact.php?status=thanks");
+    exit;
 }
 ?><?php 
 
